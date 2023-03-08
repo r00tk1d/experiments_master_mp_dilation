d414658afac016a2a32e8da6b1dc6f775a7f880b
hirschmi <hirschmi@hu-berlin.de>
2023-03-08 10:43:11 +0100

    adjust non overlapping longest chain to stumpy way

--- usecases/helper/utils.py
+++ usecases/helper/utils.py
@@ -17,17 +17,22 @@ def remove_overlapping_chains(all_chain_set, m, d):
     all_non_overlapping_chain_set = []
     non_overlapping_unanchored_chain = []
     for i in range(len(all_chain_set)):
-        unique = True
+        non_overlap = True
         for j in range(1, len(all_chain_set[i])):
-            unique = _check_unique(
-                all_chain_set[i][j-1], all_chain_set[i][j], w)
-            if not unique:
+            non_overlap = _check_non_overlap(all_chain_set[i][j-1], all_chain_set[i][j], w)
+            if not non_overlap:
                 break
-        if unique:
+        if non_overlap:
             all_non_overlapping_chain_set.append(all_chain_set[i])
-            if len(all_chain_set[i]) > len(non_overlapping_unanchored_chain):
-                non_overlapping_unanchored_chain = all_chain_set[i]
+            non_overlapping_unanchored_chain = _set_best_chain(non_overlapping_unanchored_chain, all_chain_set[i])
     return all_non_overlapping_chain_set, non_overlapping_unanchored_chain
 
-def _check_unique(seq_one, seq_two, w):
+def _check_non_overlap(seq_one, seq_two, w):
     return seq_one + w < seq_two
+
+def _set_best_chain(current_longest_chain, new_chain):
+    if len(new_chain) > len(current_longest_chain):
+        return new_chain
+    elif len(new_chain) == len(current_longest_chain) and new_chain[0] < current_longest_chain[0]:
+        return new_chain
+    return current_longest_chain
